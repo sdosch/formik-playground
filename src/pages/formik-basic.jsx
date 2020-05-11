adb14f28a6f204793712a323b66b4963db0eae2a
Stefan <stefan.dosch@muenchenerhyp.de>
2020-05-11 10:33:48 +0200

    initial files

---
+++ src/pages/formik-basic.jsx
@@ -0,0 +1,69 @@
+// Render Prop
+import React from "react";
+import { Formik, Field, ErrorMessage } from "formik";
+import {
+  Button,
+  Form,
+  Jumbotron,
+  InputGroup,
+  FormControl,
+} from "react-bootstrap";
+
+const Basic = () => (
+  <Jumbotron>
+    <h1>Any place in your app!</h1>
+    <Formik
+      initialValues={{ email: "", password: "" }}
+      validate={(values) => {
+        const errors = {};
+        if (!values.email) {
+          errors.email = "Required";
+        } else if (
+          !/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i.test(values.email)
+        ) {
+          errors.email = "Invalid email address";
+        }
+        return errors;
+      }}
+      onSubmit={(values, { setSubmitting }) => {
+        setTimeout(() => {
+          alert(JSON.stringify(values, null, 2));
+          setSubmitting(false);
+        }, 400);
+      }}
+    >
+      {({ isSubmitting }) => (
+        <Form>
+          <InputGroup className="mb-3">
+            <InputGroup.Prepend>
+              <InputGroup.Text id="basic-addon1">@</InputGroup.Text>
+            </InputGroup.Prepend>
+            <FormControl
+              placeholder="Username"
+              aria-label="Username"
+              aria-describedby="basic-addon1"
+              type="email"
+              name="email"
+            />
+            <ErrorMessage name="email" />
+          </InputGroup>
+
+          <Form.Group controlId="formBasicPassword">
+            <Form.Label>Password</Form.Label>
+            <Form.Control
+              type="password"
+              placeholder="Password"
+              name="password"
+            />
+          </Form.Group>
+          <ErrorMessage name="password" component="div" />
+          <Button type="submit" disabled={isSubmitting}>
+            Submit
+          </Button>
+        </Form>
+      )}
+    </Formik>
+  </Jumbotron>
+);
+
+export default Basic;
---
+++ src/pages/formik-basicform.jsx
@@ -0,0 +1,156 @@
+import React from "react";
+import { Form, Button, Container } from "react-bootstrap";
+import { Formik, ErrorMessage } from "formik";
+import * as Yup from "yup";
+
+// RegEx for phone number validation
+const phoneRegExp = /^(\+?\d{0,4})?\s?-?\s?(\(?\d{3}\)?)\s?-?\s?(\(?\d{3}\)?)\s?-?\s?(\(?\d{4}\)?)?$/;
+
+// Schema for yup
+const validationSchema = Yup.object().shape({
+  name: Yup.string()
+    .min(2, "Names must have at least 2 characters")
+    .max(100, "Names can't be longer than 100 characters")
+    .required("Name is required"),
+  email: Yup.string()
+    .email("Must be a valid email address")
+    .max(100, "Email must be less than 100 characters")
+    .required("Email is required"),
+  phone: Yup.string()
+    .matches(phoneRegExp, "Phone number is not valid")
+    .required("Phone number required"),
+  blog: Yup.string()
+    .url("Must enter URL in http://www.example.com format")
+    .required("URL required"),
+});
+
+const BasicForm = () => {
+  return (
+    <Container>
+      <h1>Basic Form</h1>
+      <Formik
+        initialValues={{ name: "", email: "", phone: "", blog: "" }}
+        validationSchema={validationSchema}
+        onSubmit={(values, { setSubmitting, resetForm }) => {
+          // When button submits form and form is in the process of submitting, submit button is disabled
+          setSubmitting(true);
+
+          // Simulate submitting to database, shows us values submitted, resets form
+          setTimeout(() => {
+            alert(JSON.stringify(values, null, 2));
+            resetForm();
+            setSubmitting(false);
+          }, 500);
+        }}
+      >
+        {/* Callback function containing Formik state and helpers that handle common form actions */}
+        {({
+          values,
+          errors,
+          touched,
+          handleChange,
+          handleBlur,
+          handleSubmit,
+          isSubmitting,
+        }) => (
+          <Form onSubmit={handleSubmit} className="mx-auto">
+            {console.log(values)}
+            <Form.Group controlId="formName">
+              <Form.Label>Name:</Form.Label>
+              <Form.Control
+                type="text"
+                name="name"
+                placeholder="Full Name"
+                onChange={handleChange}
+                onBlur={handleBlur}
+                value={values.name}
+                className={touched.name && errors.name ? "border-danger" : null}
+              />
+              <ErrorMessage
+                name="name"
+                component={Form.Text}
+                className="text-danger"
+              />
+            </Form.Group>
+            <Form.Group controlId="formEmail">
+              <Form.Label>Email:</Form.Label>
+              <Form.Control
+                type="text"
+                name="email"
+                placeholder="Email"
+                onChange={handleChange}
+                onBlur={handleBlur}
+                value={values.email}
+                className={
+                  touched.email && errors.email ? "border-danger" : null
+                }
+              />
+              <ErrorMessage
+                name="email"
+                component={Form.Text}
+                className="text-danger"
+              />
+            </Form.Group>
+            <Form.Group controlId="formPhone">
+              <Form.Label>Phone:</Form.Label>
+              <Form.Control
+                type="text"
+                name="phone"
+                placeholder="Phone"
+                onChange={handleChange}
+                onBlur={handleBlur}
+                value={values.phone}
+                className={
+                  touched.phone && errors.phone ? "border-danger" : null
+                }
+              />
+              <ErrorMessage
+                name="phone"
+                component={Form.Text}
+                className="text-danger"
+              />
+            </Form.Group>
+            <Form.Group controlId="formBlog">
+              <Form.Label>Blog:</Form.Label>
+              <Form.Control
+                type="text"
+                name="blog"
+                placeholder="Blog URL"
+                onChange={handleChange}
+                onBlur={handleBlur}
+                value={values.blog}
+                className={touched.blog && errors.blog ? "border-danger" : null}
+              />
+              <ErrorMessage
+                name="blog"
+                component={Form.Text}
+                className="text-danger"
+              />
+            </Form.Group>
+            <Form.Group controlId="formSelect">
+              <Form.Label>Example select:</Form.Label>
+              <Form.Control as="select">
+                <option selected disabled>
+                  please select...
+                </option>
+                <option>Apples</option>
+                <option>Monkeys</option>
+                <option>Watermelons</option>
+              </Form.Control>
+            </Form.Group>
+            <Form.Group controlId="formSwitch">
+              <Form.Label>Example switch:</Form.Label>
+              <Form.Switch label="Check this switch" />
+            </Form.Group>
+
+            <Button variant="primary" type="submit" disabled={isSubmitting}>
+              Submit
+            </Button>
+          </Form>
+        )}
+      </Formik>
+    </Container>
+  );
+};
+
+export default BasicForm;
